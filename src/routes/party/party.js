e8f2b022a32f9d96d765fcb7d2e070f9e3583a98
Daniel Guerrero <danxguerrero@google.com>
2024-02-22 12:26:44 -0500

    Created route to leave party.

--- src/routes/party/party.js
+++ src/routes/party/party.js
@@ -21,4 +21,55 @@ router.get('/', requiresAuth(), async (req, res, next) => {
   }
 })
 
+router.put("/leave/:partyId", async (req, res, next) => {
+  try {
+      // Find Party
+    const party = await Party.findByPk(req.params.partyId, {
+      include: [
+        {
+          model: User,
+          attributes: ['id']
+        }
+      ]
+    })
+
+    if (!party) {
+      return res.sendStatus(404)
+    }
+
+    // Change active field to false
+    await party.update({active: false})
+
+    // Find user1 
+    const user1 = await User.findByPk(party.Users[0].id, 
+    //   {
+    //   include: {
+    //     model: Matcher,
+    //     attributes: ['activeMatcher']
+    //   }
+    // }
+    )
+    // Find user2
+    const user2 = await User.findByPk(party.Users[1].id,
+    //   {
+    //   include: {
+    //     model: Matcher,
+    //     attributes: ['activeMatcher']
+    //   }
+    // }
+    )
+    
+    await user1.update({activeParty: false})
+    await user2.update({activeParty: false})
+
+    res.sendStatus(200)
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+
+
 module.exports = router
