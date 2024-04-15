6697e4d107ae0593e617e66d989a528216ed6016
Aaron Sanchez <saarong12@gmail.com>
2024-04-15 14:12:09 -0500

    Added support for front end auth.
    Changed the env variables and the middleware function to get the users info from the headers.

--- src/app.js
+++ src/app.js
@@ -4,35 +4,36 @@ const express = require('express')
 const app = express()
 const { auth } = require('express-oauth2-jwt-bearer')
 const { userAuth } = require('./middleware')
-
+const cors = require('cors')
 const { AUTH0_SECRET, AUTH0_AUDIENCE, AUTH0_BASE_URL, AUTH0_SIGNING_ALGO } =
   process.env
 
-const jwtCheck = {
+const jwtCheck = auth({
   secret: AUTH0_SECRET,
   audience: AUTH0_AUDIENCE,
   issuerBaseURL: AUTH0_BASE_URL,
   tokenSigningAlg: AUTH0_SIGNING_ALGO
-}
+})
 
-app.use(auth(jwtCheck))
+app.use(cors())
+app.use(jwtCheck)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// app.use(userAuth)
+app.use(userAuth)
 
-app.get('/test', (req, res, next) => {
-  res.send({ msg: 'Hello World' })
-})
+// app.get('/test', (req, res, next) => {
+//   res.send({ msg: 'Hello World' })
+// })
 
-// // User routes (mainly for manual testing)
-// app.use('/api/users', userRouter)
+// User routes (mainly for manual testing)
+app.use('/api/users', userRouter)
 
-// // Matcher routes
-// app.use('/api/matchers', matcherRouter)
+// Matcher routes
+app.use('/api/matchers', matcherRouter)
 
-// // Party routes
-// app.use('/api/parties', partyRouter)
+// Party routes
+app.use('/api/parties', partyRouter)
 
 module.exports = app
